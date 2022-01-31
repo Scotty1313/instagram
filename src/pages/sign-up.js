2b66bea6a8168c10ffa009b16ef1b49f1aaebbff
Scotty1313 <63591088+Scotty1313@users.noreply.github.com>
2022-01-31 12:08:08 +0100

    Update sign-up.js
    Update for deprecated useHistory => useNavigate

--- src/pages/sign-up.js
+++ src/pages/sign-up.js
@@ -1,11 +1,11 @@
 import { useState, useContext, useEffect } from 'react';
-import { Link, useHistory } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import FirebaseContext from '../context/firebase';
 import * as ROUTES from '../constants/routes';
 import { doesUsernameExist } from '../services/firebase';
 
 export default function SignUp() {
-  const history = useHistory();
+  const history = useNavigate();
   const { firebase } = useContext(FirebaseContext);
 
   const [username, setUsername] = useState('');
